d081d3dd3c20ed467f0b5ca9d8de7658ab059850
konstantin <konstantin.chernyshov@bostongene.com>
2022-01-11 08:07:44 +0300

    v0.5 independent models

--- app/__init__.py
+++ app/__init__.py
@@ -3,9 +3,11 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
+from app.models import Base
+
 app = Flask(__name__)
 app.config.from_object(Config)
-db = SQLAlchemy(app)
+db = SQLAlchemy(app, model_class=Base)
 migrate = Migrate(app, db)
 
 from app import routes, models
--- app/models.py
+++ app/models.py
@@ -1,21 +1,35 @@
 from datetime import datetime
-from app import db
+from sqlalchemy.orm import relationship
+from sqlalchemy import (
+    DateTime,
+    Column,
+    Integer,
+    String,
+    ForeignKey
+)
+from sqlalchemy.ext.declarative import declarative_base
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), index=True, unique=True)
-    email = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
-    posts = db.relationship('Post', backref='author', lazy='dynamic')
+Base = declarative_base()
+
+
+class User(Base):
+    __tablename__ = "user"
+    id = Column(Integer, primary_key=True)
+    username = Column(String(64), index=True, unique=True)
+    email = Column(String(120), index=True, unique=True)
+    password_hash = Column(String(128))
+    posts = relationship('Post', backref='author', lazy='dynamic')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class Post(Base):
+    __tablename__ = "post"
+    id = Column(Integer, primary_key=True)
+    body = Column(String(140))
+    timestamp = Column(DateTime, index=True, default=datetime.utcnow)
+    user_id = Column(Integer, ForeignKey('user.id'))
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
