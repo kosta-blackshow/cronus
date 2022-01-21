d1595013e837c8f4b4e899290e1d720cc45f8412
konstantin <konstantin.chernyshov@bostongene.com>
2022-01-21 16:15:35 +0300

    v0.16 docker-compose

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-
-#set -e
-#
-#host="$1"
-#shift
-#
-#until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "postgres" -c '\q'; do
-#  >&2 echo "Postgres is unavailable - sleeping"
-#  sleep 1
-#done
-#
-#>&2 echo "Postgres is up - executing command"
-
+# this script is used to boot a Docker container
 source venv/bin/activate
-flask db upgrade
 exec gunicorn -b :5000 --access-logfile - --error-logfile - backend:server
