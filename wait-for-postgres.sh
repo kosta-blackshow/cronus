f94363f3e65fcbbf7415b24ba6753aaec2d592c6
konstantin <konstantin.chernyshov@bostongene.com>
2022-01-21 14:36:28 +0300

    v0.15 update script

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -14,4 +14,4 @@
 
 source venv/bin/activate
 flask db upgrade
-exec gunicorn -b :5000 --access-logfile - --error-logfile - backend:cronus
+exec gunicorn -b :5000 --access-logfile - --error-logfile - backend:server
