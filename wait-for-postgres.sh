54e53be6d1bf3abad29d46d21088fed2ab902c08
konstantin <konstantin.chernyshov@bostongene.com>
2022-01-22 00:38:13 +0300

    v0.17 wait-for-postgres

--- wait-for-postgres.sh
+++ wait-for-postgres.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 # wait-for-postgres.sh
 
-set -e
+#set -e
 
-until pg_isready --username=postgres --host=postgres; do
-  >&2 echo "Postgres is unavailable - sleeping"
-  sleep 1
-done
-
->&2 echo "Postgres is up - executing command"
-psql --username=postgres --host=postgres --list
+#until pg_isready --username=postgres --host=postgres; do
+#  >&2 echo "Postgres is unavailable - sleeping"
+#  sleep 1
+#done
+#
+#>&2 echo "Postgres is up - executing command"
+#psql --username=postgres --host=postgres --list
 
 . venv/bin/activate
 flask db upgrade
