90d12d5f6553ee6a2d952b4b1fac584a6b892036
konstantin <konstantin.chernyshov@bostongene.com>
2022-01-24 14:29:01 +0300

    v0.24 sqlalchemy echo = true

--- backend/config.py
+++ backend/config.py
@@ -6,3 +6,4 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                               'sqlite:///' + os.path.join(basedir, '../app.db') + '?check_same_thread=False'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_ECHO = os.environ.get('SQLALCHEMY_ECHO') or False
